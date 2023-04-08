3ca9ca9ea1ed3066a82844fe8c0e3032db683bc4
Rodz Andrie Amor <ramorstem2022@example.com>
2023-04-07 20:09:54 -0400

    Text to mp3 working

--- mp3totext.py
+++ mp3totext.py
@@ -1,5 +1,11 @@
+import os
 import openai
 
-audio_file = open()
+openai.api_key = os.getenv("OPEN_API_KEY")
 
-#test
+# Keep in mind the audiofile should be less than 25 mb
+# If you want an input longer than 25 mb, use the pydub library
+audio_file = open("test.mp3", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
+print(transcript)
+
