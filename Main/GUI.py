0197189179502bcf1c0800907050d08e68c1a32e
Rodz Andrie Amor <ramorstem2022@example.com>
2023-04-09 07:43:31 -0400

    Added a reset button to GUI.py and updated to better UI

--- Main/GUI.py
+++ Main/GUI.py
@@ -24,13 +24,33 @@ BATCHES = []
 # Window Size
 window.geometry("{0}x{1}+0+0".format(window.winfo_screenwidth(), window.winfo_screenheight()))
 
+def placeStartWidgets():
+    title.place(relx=0.5, rely=0.12, anchor=CENTER)
+    instructions.place(relx=0.5, rely=0.22, anchor=CENTER)
+    submitButton.place(relx=0.5, rely=0.87, anchor=CENTER)
+    validateLabel.place(relx=0.5, rely=0.94, anchor=CENTER)
+
+    fileFrame.place(relx=0.2, rely=0.55, anchor=CENTER)
+    fileLabel.place(relx=0.5, rely=0.2, anchor=CENTER)
+    browseFileButton.place(relx=0.5, rely=0.5, anchor=CENTER)
+    fileNameLabel.place(relx=0.5, rely=0.8, anchor=CENTER)
+
+    textFrame.place(relx=0.5, rely=0.55, anchor=CENTER)
+    textBoxLabel.place(relx=0.5, rely=0.15, anchor=CENTER)
+    textBox.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+    linkFrame.place(relx=0.8, rely=0.55, anchor=CENTER)
+    youtubeLabel.place(relx=0.5, rely=0.2, anchor=CENTER)
+    youtubeTextBox.place(relx=0.5, rely=0.5, anchor=CENTER)
+    youtubeIcon.place(relx=0.5, rely=0.8, anchor=CENTER)
+
 def browseFiles():
     filePath = filedialog.askopenfilename(initialdir="./",
                                           title="Select a file",
                                           filetypes=(("Mp3 Files", "*.mp3"),
                                                      ("MP4 files", "*.mp3")))
-    print(filePath)
     fileNameLabel.configure(text=filePath)
+
 
 def validateInput():
     global OPTION
@@ -64,32 +84,37 @@ def validateInput():
             OPTION = 2
             YOUTUBE_URL = youtubeTextBox.get("1.0", END)
         
-        # Delete the widgetsÇ
+        # Delete the widgets
         for widgets in window.winfo_children():
-            widgets.destroy()
+            widgets.place_forget()
         
         createNewScreen()
 
+def resetWindow():
+    for widgets in window.winfo_children():
+            widgets.place_forget()
+    
+    placeStartWidgets()
+
 def createNewScreen():
     global BATCHES
-    # print(OPTION)
     (BATCHES, summaries) = populate_summaries(OPTION, FILE_PATH, RAW_TRANSCRIPT, YOUTUBE_URL)
-    # print(BATCHES)
 
-
-    # Modify the commands to the corresponding method
-    summaryButton = ct.CTkButton(window, text="Summary", width=250, height=60, font=ct.CTkFont(family="Helvetica", size=20, weight="bold"), command=lambda: summary(summaries[1], outputTextBox))
-    notesButton = ct.CTkButton(window, text="Notes", width=250, height=60, font=ct.CTkFont(family="Helvetica", size=20, weight="bold"), command=lambda: notes(' '.join(summaries[1]), outputTextBox))
-    practiceButton = ct.CTkButton(window, text="Practice Problems", width=250, height=60, font=ct.CTkFont(family="Helvetica", size=20, weight="bold"), command=lambda: practce_problems(' '.join(summaries[1]), outputTextBox))
+    summaryButton = ct.CTkButton(window, text="Summary", width=250, height=60, font=ct.CTkFont(family="Helvetica", size=20, weight="bold"), command=lambda: summary(summaries, outputTextBox))
+    notesButton = ct.CTkButton(window, text="Notes", width=250, height=60, font=ct.CTkFont(family="Helvetica", size=20, weight="bold"), command=lambda: notes(' '.join(summaries), outputTextBox))
+    practiceButton = ct.CTkButton(window, text="Practice Problems", width=250, height=60, font=ct.CTkFont(family="Helvetica", size=20, weight="bold"), command=lambda: practce_problems(' '.join(summaries), outputTextBox))
+    resetButton = ct.CTkButton(window, text="New Lecture", width=250, height=60, font=ct.CTkFont(family="Helvetica", size=20, weight="bold"), fg_color="red", command=resetWindow)
 
     ouputLabel = ct.CTkLabel(window, text="Output", font=ct.CTkFont(family="Helvetica", size=30, weight="bold"))
     outputTextBox = ct.CTkTextbox(window, width=500, height=500, font=ct.CTkFont(family="Helvetica", size=20, weight="bold"))
 
-    summaryButton.place(relx=0.2, rely=0.3)
-    notesButton.place(relx=0.2, rely=0.5)
-    practiceButton.place(relx=0.2, rely=0.7)
-
     outputTextBox.place(relx=0.5, rely=0.2)
+
+    summaryButton.place(in_=outputTextBox, relx=0.0, rely=0.0, x=-400, y = 0)
+    notesButton.place(in_=outputTextBox, relx=0.0, rely=0.0, x=-400, y=120)
+    practiceButton.place(in_=outputTextBox, relx=0.0, rely=0.0, x=-400, y=240)
+    resetButton.place(in_=outputTextBox, relx=0.0, rely=0.0, x=-400, y=360)
+
     ouputLabel.place(in_=outputTextBox, relx=0.5, rely=0.0, y=-80, anchor="n")
 
 # Cleans the file after closing
@@ -109,7 +134,6 @@ def closeWindow():
 
     window.destroy()
     
-
 title = ct.CTkLabel(window, text="Welcome to LectureGPT", font=ct.CTkFont(family="Helvetica", size=40, weight="bold"))
 instructions = ct.CTkLabel(window, text="Choose your input", font=ct.CTkFont(family="Helvetica", size=20, weight="bold"))
 submitButton = ct.CTkButton(window, text="Submit", width=200, height=40, font=ct.CTkFont(family="Helvetica", size=30, weight="bold"), command=validateInput)
