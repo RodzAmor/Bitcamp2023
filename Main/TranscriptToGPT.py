415ff455dae39bf37d6f2a53984a04d38db9a6dc
mehdi <mehdiyev.ayyhan@gmail.com>
2023-04-09 01:28:27 -0400

    Added compatibility for transcripts longer than the max input length

--- Main/TranscriptToGPT.py
+++ Main/TranscriptToGPT.py
@@ -1,40 +1,94 @@
 import os
 import openai
+import math
+from PrepareTranscript import batches
 from dotenv import load_dotenv
 
 load_dotenv()
 openai.api_key = os.getenv("OPEN_API_KEY")
 
-audio_file = open("test.mp3", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
+#audio_file = open("test.mp3", "rb")
+#transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-while (True):
-    user_prompt = input("Would you like a summary, notes, or practice problems? ").lower()
-    gpt_prompt = "Give me "
 
-    if (user_prompt == "summary"):
-        gpt_prompt += "a 3-sentence long summary based on the following transcript:\n\n"
-    elif (user_prompt == "notes"):
-        gpt_prompt += "a whole page's worth of nicely formatted notes, using proper spacing, titles, and bullet points based on the following transcript:\n\n" 
-    elif (user_prompt == "practice problems"):
-        gpt_prompt += "practice problems and an answer key for the problems based on the following transcript:\n\n"
-    else:
-        print("That is not an option!")
-        exit(0)
 
-    gpt_prompt += transcript.text        
+def main():
+    summaries = []
+    for batch in batches:
+        with open(batch, "r") as f:
+            summaries.append(createPartitionedSummary(str(f.read())))
+    #summary = stichSummaries(summaries)
+    #print("Summary:\n------------\n" + summary)
+    summary(summaries)
+    notes(' '.join(summaries))
+    practceProblems(' '.join(summaries))
 
+    for batch in batches:
+        os.remove(batch)
+    
+
+
+
+def practceProblems(summary):
+    gpt_prompt = "Give me a few practice problems and an answer key for the problems based on the following summary:\n\n" + summary
     response = openai.Completion.create(
         model = "text-davinci-003",
         prompt = gpt_prompt,
         temperature = 0.5,
         max_tokens = 1000
     )
-    print(response.choices[0].text)
+    with open("Practice_Problems.txt", "w") as f:
+        f.write(response.choices[0].text)
+    return response.choices[0].text
+
+def notes(summary):
+    gpt_prompt = "Give me a whole page's worth of nicely formatted notes, using proper spacing, titles, and bullet points based on the following summary:\n\n" + summary
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = gpt_prompt,
+        temperature = 0.5,
+        max_tokens = 1000
+    )
+    with open("Notes.txt", "w") as f:
+        f.write(response.choices[0].text)
+    return response.choices[0].text
+
+def summary(summaries):
+    transcript = ""
+    for summary in summaries:
+        transcript += summary
+    with open("Summary.txt", "w") as f:
+        f.write(transcript)
+    return transcript
 
 
+def stichSummaries(summaries):
+    transcript = ""
+    for summary in summaries:
+        transcript += summary
+    print(transcript)
+    gpt_prompt = "Give me an 8-sentence long summary that is very detailed based on this transcript:\n\n""" + transcript
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = gpt_prompt,
+        temperature = 0.5,
+        max_tokens = 1000
+    )
+    return response.choices[0].text
 
+def createPartitionedSummary(partitionedTranscript):
+    maxChar = math.floor(2000 / len(batches)) # 2000 is the safe amount for max character length
+    gpt_prompt = "Give me a summary that has a hard max of " + str(maxChar) + """ characters, but try to  
+                 "make it as detailed as possible for this transcript (don't mention that this is a transcript summary):\n\n""" + partitionedTranscript
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = gpt_prompt,
+        temperature = 0.5,
+        max_tokens = 1000
+    )
+    return response.choices[0].text
 
+main()
 
 
 
